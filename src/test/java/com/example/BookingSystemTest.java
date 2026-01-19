8e839cab4c98b5c65fea3af224e956645dc98d05
WHITEROSE <firasmoussa60@gmail.com>
2026-01-19 10:31:31 +0100

    test a successful room booking

---
+++ src/test/java/com/example/BookingSystemTest.java
@@ -0,0 +1,63 @@
+package com.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("BookingSystem Tests")
+class BookingSystemTest {
+    @Mock private TimeProvider timeProvider;
+    @Mock private NotificationService notificationService;
+    @Mock private RoomRepository roomRepository;
+    @Mock private Room mockRoom;
+
+    @InjectMocks
+    private BookingSystem bookingSystem;
+
+
+    private LocalDateTime now;
+    private LocalDateTime start;
+    private LocalDateTime end;
+
+    @BeforeEach
+    void mockData() {
+        now = LocalDateTime.of(2026, 1, 19, 10, 0);
+        start = LocalDateTime.of(2026, 1, 19, 13, 0);
+        end = LocalDateTime.of(2026, 1, 19, 14, 0);
+    }
+
+
+    @Test
+    @DisplayName("bookRoom should return true if booking succeeded")
+    void test_bookRoom_success() {
+        when(timeProvider.getCurrentTime()).thenReturn(now);
+        when(roomRepository.findById("room-id")).thenReturn(Optional.of(mockRoom));
+        when(mockRoom.isAvailable(start, end)).thenReturn(true);
+
+        boolean result = bookingSystem.bookRoom("room-id", start, end);
+
+        assertThat(result).isTrue();
+        verify(mockRoom).addBooking(any(Booking.class));
+        verify(roomRepository).save(mockRoom);
+    }
+
+    // TODO: Test:
+    // - Unavailable room
+    // - An room that does not exist
+    // - Null parameters
+    // - startTime before currentTime
+    // - endTime before startTime
+    // - failed sendBookingConfirmation should still book
+
+}
