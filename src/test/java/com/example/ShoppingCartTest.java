4e493f4ab8854d79044ee12bde4b12aaa468a1ca
WHITEROSE <firasmoussa60@gmail.com>
2026-01-24 18:50:45 +0100

    store item in cart

--- src/test/java/com/example/ShoppingCartTest.java
+++ src/test/java/com/example/ShoppingCartTest.java
@@ -3,11 +3,16 @@ package com.example;
 import com.example.shop.Item;
 import com.example.shop.ShoppingCart;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 public class ShoppingCartTest {
 
@@ -18,9 +23,22 @@ public class ShoppingCartTest {
         cart = new ShoppingCart();
     }
 
+    @DisplayName("adds an item to the cart")
     @Test
     void addItemToCart() {
         Item item = new Item("name", BigDecimal.valueOf(250.0));
         boolean result = cart.addItem(item);
-        assertThat(result).isTrue();}
+        assertThat(result).isTrue();
+    }
+
+    @DisplayName("stores added item in the cart")
+    @Test
+    void cartContainsItem() {
+        Item item = new Item("name", BigDecimal.valueOf(250.0));
+        cart.addItem(item);
+
+        List<Item> cartItems = cart.getItems();
+
+        assertThat(cartItems).containsExactly(item);
+    }
 }
--- src/test/java/com/example/shop/ShoppingCart.java
+++ src/test/java/com/example/shop/ShoppingCart.java
@@ -1,10 +1,20 @@
 package com.example.shop;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShoppingCart {
+    private final List<Item> items = new ArrayList<>();
+
     public ShoppingCart() {
     }
 
     public boolean addItem(Item item) {
+        items.add(item);
         return true;
+    }
+
+    public List<Item> getItems() {
+        return items;
     }
 }
